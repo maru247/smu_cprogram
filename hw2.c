f62e5e4584d3e3e3afce303d5c0e9c07dea8a18a
maru247 <aleph5763@gmail.com>
2022-09-15 14:18:14 +0900

    hw2.c

---
+++ hw2.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	
+	double km;
+	printf("Please enter kilometers: ");
+	scanf("%lf", &km);
+	printf("%.1lf km is equal to %.1lf miles.", km, km / 1.609);
+	return 0;
+}
